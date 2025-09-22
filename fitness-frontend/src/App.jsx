a75744f44da293fd102c1a603135d0341e5540ed
Govindraj Ingle <govindrajingle@gmail.com>
2025-09-22 21:19:27 +0530

    set up auth config for user

--- fitness-frontend/src/App.jsx
+++ fitness-frontend/src/App.jsx
@@ -1,21 +1,33 @@
-import {
-  BrowserRouter as Router,
-  Navigate,
-  Route,
-  Routes,
-  useLocation,
-} from "react-router";
-
-import Button from "@mui/material/Button"; // if using MUI
+import {BrowserRouter as Router,} from "react-router";
+import Button from "@mui/material/Button";
+import {useContext, useEffect, useState} from "react";
+import {AuthContext} from "react-oauth2-code-pkce";
+import {useDispatch} from "react-redux";
+import {setCredentials} from "./store/authSlice.js"; // if using MUI
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Button variant="contained">Log in</Button>
-      </Router>
-    </>
-  );
+    const {token, tokenData, logIn} = useContext(AuthContext);
+    const dispatch = useDispatch();
+    const [authReady, setAuthReady] = useState(false);
+    useEffect(() => {
+        if (token) {
+            dispatch(setCredentials({token, user: tokenData}));
+            setAuthReady(true);
+        }
+    }, [token, tokenData, dispatch]);
+    return (<>
+        <Router>
+            {!token ? (<Button variant="contained" onClick={() => {
+                logIn();
+            }}>
+                LOGIN
+            </Button>) : (<div>
+                        <pre>
+                            {JSON.stringify(tokenData, null, 2)}
+                        </pre>
+            </div>)}
+        </Router>
+    </>);
 }
 
 export default App;
--- fitness-frontend/src/main.jsx
+++ fitness-frontend/src/main.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { Provider } from "react-redux";
-import { store } from "./store/store";
+import {Provider} from "react-redux";
+import {store} from "./store/store";
 
 import App from "./App";
+import {AuthProvider} from "react-oauth2-code-pkce";
+import {authConfig} from "./authConfig.js";
 
-// As of React 18
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+root.render(<AuthProvider authConfig={authConfig}
+                          loadingComponent={<div>Loading...</div>}>
+    <Provider store={store}>
+        <App/>
+    </Provider>
+</AuthProvider>);
