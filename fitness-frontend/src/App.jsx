b3bf2128ea55bd6e0885631b636783b7bb6fa8de
Govindraj Ingle <govindrajingle@gmail.com>
2025-09-23 20:51:52 +0530

    ai details page

--- fitness-frontend/src/App.jsx
+++ fitness-frontend/src/App.jsx
@@ -1,52 +1,63 @@
-import {BrowserRouter as Router, Route, Routes,} from "react-router";
+import {BrowserRouter as Router, Route, Routes, useNavigate} from "react-router";
 import Button from "@mui/material/Button";
 import {useContext, useEffect, useState} from "react";
 import {AuthContext} from "react-oauth2-code-pkce";
 import {useDispatch} from "react-redux";
-import {logout, setCredentials} from "./store/authSlice.js";
+import {logout, setCredentials} from "./store/authSlice";
 import {Box} from "@mui/material";
-import ActivityForm from "./components/ActivityForm.jsx";
-import ActivityList from "./components/ActivityList.jsx";
-import ActivityDetail from "./components/ActivityDetail.jsx"; // if using MUI
+import ActivityForm from "./components/ActivityForm";
+import ActivityList from "./components/ActivityList";
+import ActivityDetail from "./components/ActivityDetail"; // if using MUI
 
 const ActivityPage = () => {
     return (<Box sx={{flexGrow: 1}}>
-        <ActivityForm onActivitiesAdded={() => window.location.reload()}/>
+        <ActivityForm onActivityAdded={() => window.location.reload()}/>
         <ActivityList/>
-    </Box>)
-}
+    </Box>);
+};
+
+// A small wrapper so we can use navigate inside
+const GoToActivitiesButton = () => {
+    const navigate = useNavigate();
+    return (<Button
+        variant="contained"
+        onClick={() => navigate("/activities")}
+    >
+        Go to Activities
+    </Button>);
+};
 
 function App() {
     const {token, tokenData, logIn} = useContext(AuthContext);
     const dispatch = useDispatch();
     const [authReady, setAuthReady] = useState(false);
+
     useEffect(() => {
         if (token) {
             dispatch(setCredentials({token, user: tokenData}));
             setAuthReady(true);
         }
     }, [token, tokenData, dispatch]);
-    return (<>
-        <Router>
-            {!token ? (<Button variant="contained" onClick={() => {
-                logIn();
-            }}>
-                LOGIN
-            </Button>) : (<div>
-                        <pre>
-                            <Box component="section" sx={{p: 2, border: '1px dashed grey'}}>
-                                <Button variant="contained" onClick={logout}>
-                                    LOGOUT
-                                </Button>
-                                <Routes>
-                                    <Route path="/activities" element={<ActivityPage/>}/>
-                                    <Route path="/activities/:id" element={<ActivityDetail/>}/>
-                                </Routes>
-                            </Box>
-                        </pre>
-            </div>)}
-        </Router>
-    </>);
+
+    return (<Router>
+        {!token ? (<Button variant="contained" onClick={() => logIn()}>
+            LOGIN
+        </Button>) : (<div>
+                    <pre>
+                        <Box component="section" sx={{p: 2, border: "1px dashed grey"}}>
+                            <Button variant="contained" onClick={logout}>
+                                LOGOUT
+                            </Button>
+                            <Routes>
+                                <Route path="/activities" element={<ActivityPage/>}/>
+                                <Route path="/activities/:id" element={<ActivityDetail/>}/>
+                            </Routes>
+                        </Box>
+                    </pre>
+            {/* Second button goes here */}
+            <GoToActivitiesButton/>
+        </div>)}
+    </Router>);
 }
 
 export default App;
